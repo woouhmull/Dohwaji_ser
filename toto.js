87ba82d1a0fcdc4d70da9c5b817d7fdd2e9d082a
ssdd6110@ajou.ac.kr <ssdd6110@ajou.ac.kr>
2021-09-02 20:58:19 +0900

    fianls

--- toto.js
+++ toto.js
@@ -11,6 +11,6 @@ app.listen(3001, () => {
 
     console.log('서버 실행중')
     setTimeout(() => console.log('Data in'),7000);
-    setTimeout(() => console.log('37'),1000);
-    
+    setTimeout(() => console.log('37'),8000);
+
 })
