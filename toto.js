d521fb423b27f9c2e72a25426657cc8cb3d3ace1
ssdd6110@ajou.ac.kr <ssdd6110@ajou.ac.kr>
2021-09-02 20:55:09 +0900

    fianls

--- toto.js
+++ toto.js
@@ -8,8 +8,9 @@ app.use(require('cors')())
 
 
 app.listen(3001, () => {
+
+    console.log('서버 실행중')
     async () =>{
-      console.log('서버 실행중')
       await sleep(7000);
       console.log('Data in')
       await sleep(1000);
